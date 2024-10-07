71288549be063658483f9ceee5910fb8a0a798fc
jefaokpta <jefaokpta@hotmail.com>
2024-10-06 21:47:31 -0300

    base ok - falta escolher template da landing

---
+++ src/views/AdminView.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>ADMIN</h1>
+  <router-link to="/" class="text-blue-500">Home</router-link>
+</template>
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup>
+</script>
+
+<template>
+  <h1>HOME</h1>
+  <router-link to="/admin" class="text-blue-500">Admin</router-link>
+</template>
